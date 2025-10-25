3fe7f8ec71c3729215c94c3f2c618ba5d6f7544e
Lahiru Anushka <lahiruanushkarathnayake@gmail.com>
2025-10-25 17:46:41 +0530

    chore: remove unused HomePageFooter import from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { ThemeProvider } from "@/components/theme-provider";
-import HomePageFooter from "@/components/HomePageFooter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
